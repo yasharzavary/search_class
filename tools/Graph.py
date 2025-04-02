91976ac288a7fffe1d64010e482bf1603b8a2b13
yashar <115088770+yasharzavary@users.noreply.github.com>
2025-04-02 18:44:36 +0330

    feat(Graph.py): start, end root control done.

--- tools/Graph.py
+++ tools/Graph.py
@@ -15,16 +15,36 @@ from Node import Node
 
 # main class
 class Graph:
-    def __init__(self, node: [str, Node] = None):
-        if isinstance(node, str):
-            if node is None:
+    have_start = False
+    have_end = False
+
+    def __init__(self, root_node: [str, Node] = None,
+                 start: bool = False,
+                 end: bool = False):
+        # control root node details.
+        if isinstance(root_node, str):
+            if root_node is None:
                 self.__root = Node()
             else:
-                self.__root = Node(node)
-        elif isinstance(node, Node):
-            self.__root = node
+                self.__root = Node(root_node)
+        elif isinstance(root_node, Node):
+            self.__root = root_node
         else:
-            raise TypeError(f'node should be {Node} or string -name- type')
+            raise TypeError(f'node should be {type(Node)} or string -name- type')     # if it isn't node type, raise.
+
+        # graph start and end control info.
+        self.__graph_start = True
+        if start:
+            self.have_start = True
+            self.__graph_start = True
+
+        self.__graph_end = True
+        if end:
+            self.have_end = True
+            self.__graph_end = True
+
+
+
 
 
 
