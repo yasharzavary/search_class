1847f03111d48bfffa753de61b0e968603bf89f4
yashar <115088770+yasharzavary@users.noreply.github.com>
2025-04-03 12:45:30 +0330

    feat: BFS search algorithm created for graphs now user can use BFS algorithm for poth check and path modes

--- tools/Graph.py
+++ tools/Graph.py
@@ -7,7 +7,7 @@ last update: April 2, 2025
 
 I will appreciate for your suggestions about this class.
 """
-from Node import Node
+from .Node import Node
 
 
 # built-in errors.
@@ -21,18 +21,19 @@ class FalseSearchMode(Exception):
 # main class
 class Graph:
 
-    def __init__(self, start_node: Node, goal_nodes: list):
+    def __init__(self, start_node: Node, goal_nodes: [list, str]):
         """
             you can create graphs and use tools of it for explore your structure.
         :param start_node:
         :param goal_nodes:
         """
+
         # check inputs
         self.__control(start_node)
         self.__control(goal_nodes)
 
         self.__startRoot = start_node
-        self.__goalRoot = goal_nodes
+        self.__goalRoot = [goal_nodes] if isinstance(goal_nodes, Node) else goal_nodes
 
 
     def __control(self, nodes: [Node, list]):
@@ -47,33 +48,78 @@ class Graph:
                 if not isinstance(node, Node): raise TypeError(f'Nodes must be {type(Node)}, {node} is not!')
         elif not isinstance(nodes, Node):
             raise TypeError(f'Nodes must be {type(Node)}, {nodes} is not!')
-        else:
-            raise TypeError(f'Nodes must be {type(Node)}.')
+
 
 
     def DFS_search(self, mode: str = 'check'):
-        if mode not in ['check', 'way']: raise FalseSearchMode('search mode must be way or check')
+        if mode not in ['check', 'path']: raise FalseSearchMode('search mode must be path or check')
         frontier = [self.__startRoot]
         visited = list()
-        way = list()
-        while frontier:
-            now = frontier.pop(0)
-            if now == '!!':
-                way.pop()
-                continue
-            elif now in visited: continue             # if we check this node before this, continue to next node
-            elif now == self.__goalRoot:
-                return f'Find, node: {now}' if mode == 'check' else ' -> '.join(way)     # return result if find
+        if mode == 'path':
+            way = list()
+            while frontier:
+                now = frontier.pop(0)
+                if now == '!!':
+                    way.pop()
+                    continue
+                elif now in visited: continue             # if we check this node before this, continue to next node
+                for goal in self.__goalRoot:
+                    if now == goal:
+                        return f'Find, node: {now}' if mode == 'check' else ' -> '.join(
+                            [node.name for node in way] + [goal.name]
+                        ), now    # return result if find
 
-            # add controling units.
-            visited.append(now)
-            way.append(now)
-            frontier = now.children + ['!!'] + frontier
+                # add controling units.
+                visited.append(now)
+                way.append(now)
+                frontier = now.children + ['!!'] + frontier
+        else:
+            while frontier:
+                now = frontier.pop(0)
+                if now in visited:
+                    continue  # if we check this node before this, continue to next node
+                elif now == self.__goalRoot:
+                    return f'goal node find', now
 
+                # add controling units.
+                visited.append(now)
+                frontier = now.children + frontier
 
+        return 'goal node doesn\'t exist'
 
+    def BFS_search(self, mode: str = 'check'):
+        if mode not in ['check', 'path']: raise FalseSearchMode('search mode must be path')
+        frontier = [self.__startRoot]
+        visited = list()
+        if mode == 'path':
+            parents = dict()
+            while frontier:
+                now = frontier.pop(0)
+                visited.append(now)
+                if now in self.__goalRoot:
+                    temp = now
+                    path = [now.name]
+                    while temp.name != '0':
+                        temp = parents[temp]
+                        path.insert(0, temp.name)
+                    return f'node exist, path: {' -> '.join(path)}', now
 
+                # analyze and add new children.
+                for child in now.children:
+                    if child not in visited:
+                        frontier.append(child)
+                        parents[child] = now
+        else:
+            while frontier:
+                now = frontier.pop(0)
+                visited.append(now)
+                if now in self.__goalRoot:
+                    return 'node exist', now
 
+                # analyze and add new children.
+                for child in now.children:
+                    if child not in visited:
+                        frontier.append(child)
 
 
 
