71b1f6a8deae3bca1b3f98ea0c1f7a5b510be029
yashar <115088770+yasharzavary@users.noreply.github.com>
2025-04-08 19:36:04 +0330

    feat: main layout created exit and cahnge button added to the view

--- tools/UI/ui.py
+++ tools/UI/ui.py
@@ -1,5 +1,11 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow, QHBoxLayout
+from PySide6.QtWidgets import (QApplication, QMainWindow, QHBoxLayout,
+                                QVBoxLayout, QWidget, QLabel,
+                                QPushButton)
+from PySide6.QtCore import Signal
+from PySide6.QtGui import QPixmap
+
+from .graphs import *
 
 class MainWindow(QMainWindow):
 
@@ -9,9 +15,29 @@ class MainWindow(QMainWindow):
         # information of page
         self.setWindowTitle("Graph")
         self.setGeometry(200, 150, 600, 700)
+        cenral_widget = QWidget()
+        self.setCentralWidget(cenral_widget)
 
-        main_layout = QHBoxLayout()
+
+        # main layout
+        main_layout = QHBoxLayout(cenral_widget)
 
 
+        # graphic scene details
+        self.scene = GraphScene()
+        self.view = GraphView(self.scene)
+        self.scene.add_node('x', 0, 0)
+
+        # tools layout
+        tool_layout = QVBoxLayout()
+        change_button = QPushButton('Change')
+        exit_button = QPushButton('Exit')
+
+        tool_layout.addWidget(change_button, alignment=Qt.AlignTop)
+        tool_layout.addWidget(exit_button, alignment=Qt.AlignBottom)
+
+        # adding to main layout
+        main_layout.addWidget(self.view)
+        main_layout.addLayout(tool_layout)
 
 
