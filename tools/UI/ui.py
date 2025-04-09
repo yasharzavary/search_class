ae2747c2523fb0979860cc1d14053442056558c3
yashar <115088770+yasharzavary@users.noreply.github.com>
2025-04-09 13:27:29 +0330

    buttons scene added for button exit, change and back, change scene added for better control

--- tools/UI/ui.py
+++ tools/UI/ui.py
@@ -7,6 +7,10 @@ from PySide6.QtGui import QPixmap
 
 from .graphs import *
 
+
+# TODO: message box in bottom of window.
+
+
 class MainWindow(QMainWindow):
 
     def __init__(self):
@@ -18,7 +22,6 @@ class MainWindow(QMainWindow):
         cenral_widget = QWidget()
         self.setCentralWidget(cenral_widget)
 
-
         # main layout
         main_layout = QHBoxLayout(cenral_widget)
 
@@ -29,15 +32,62 @@ class MainWindow(QMainWindow):
         self.scene.add_node('x', 0, 0)
 
         # tools layout
-        tool_layout = QVBoxLayout()
-        change_button = QPushButton('Change')
-        exit_button = QPushButton('Exit')
-
-        tool_layout.addWidget(change_button, alignment=Qt.AlignTop)
-        tool_layout.addWidget(exit_button, alignment=Qt.AlignBottom)
+        self.tool_layout = QVBoxLayout()
+        self.__add_main_buttons()
 
         # adding to main layout
         main_layout.addWidget(self.view)
-        main_layout.addLayout(tool_layout)
+        main_layout.addLayout(self.tool_layout)
+
+    def __exit(self):
+        self.close()
+    
+    def __change(self):
+        """
+            when user want to change the graph, this function will trigger
+        """
+        self.__remove_widgets(self.tool_layout)
+        # buttons definition
+        add_note = QPushButton('Add Node')
+        back_button = QPushButton('Back')
+
+        back_button.clicked.connect(self.__add_main_buttons)
 
 
+        self.tool_layout.addWidget(add_note, alignment = Qt.AlignTop)
+        self.tool_layout.addStretch(0)
+        self.tool_layout.addWidget(back_button, alignment=Qt.AlignBottom)
+
+
+    def __add_main_buttons(self):
+        """
+            this functino will add main buttons like change, search to the main layout
+        """
+        self.__remove_widgets(self.tool_layout)
+        change_button = QPushButton('Change')
+        search_button = QPushButton('Search')
+        exit_button = QPushButton('Exit')
+
+        # trigger button to functions
+        exit_button.clicked.connect(self.__exit)
+        change_button.clicked.connect(self.__change)
+
+        # tool_layout.setSpacing(0)
+        # tool_layout.setContentsMargins(0,0,0,0)
+        self.tool_layout.addWidget(change_button, alignment=Qt.AlignTop)
+        self.tool_layout.addWidget(search_button, alignment=Qt.AlignTop)
+        self.tool_layout.addStretch(0)
+        self.tool_layout.addWidget(exit_button, alignment=Qt.AlignBottom)
+
+
+
+    def __remove_widgets(self, layout):
+        """
+            this function is responsible to delete all widgets in one layout
+
+            :param layout: layout that we want to delete widgets of it
+        """
+        while layout.count():
+            child = layout.takeAt(0)
+            if child.widget():
+                child.widget().deleteLater()
